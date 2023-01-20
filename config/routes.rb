6fca18a9e4e243092de32e4f1fa3b132d185a45f
Shaheer Ahmad <shaheer.ahmad@square63.com>
2023-01-20 12:21:45 +0500

    Added sidekiq web ui to view metris and the details regarding jobs.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web => "/sidekiq" # mount Sidekiq::Web in your Rails app
   resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
